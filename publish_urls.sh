ff311b8ece59b41df0971dea4f010f1559942a95
abozhkov <alexander.bojkov89@gmail.com>
2024-12-24 17:50:15 +0200

    feat: Create bash cript for publishing urls to the url_queue

---
+++ publish_urls.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+QUEUE_NAME="url_queue"
+RABBITMQ_CONTAINER_NAME="rabbit_mq"
+RABBITMQ_IP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $RABBITMQ_CONTAINER_NAME)
+
+if [ "$#" -lt 1 ]; then
+  echo "Usage: $0 <url1> <url2> ... <urlN>"
+  exit 1
+fi
+
+publish_url() {
+  local url="$1"
+  curl -u guest:guest -X POST \
+    -H "content-type:application/json" \
+    -d "{\"properties\":{},\"routing_key\":\"$QUEUE_NAME\",\"payload\":\"$url\",\"payload_encoding\":\"string\"}" \
+    "http://$RABBITMQ_IP:15672/api/exchanges/%2F/amq.default/publish"
+}
+
+for url in "$@"; do
+  echo "Publishing URL: $url"
+  publish_url "$url"
+done
+
+echo "All URLs have been published to RabbitMQ."
